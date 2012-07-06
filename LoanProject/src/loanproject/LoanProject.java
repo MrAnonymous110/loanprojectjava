a7bfbc2c35a88b5980426e53de81d3116b07500d
tavantuyen92@gmail.com <tavantuyen92@gmail.com@f2ded979-f436-fa1c-38d0-fffca466db5f>
2012-07-06 13:46:42 +0000

    Test 1

--- LoanProject/src/loanproject/LoanProject.java
+++ LoanProject/src/loanproject/LoanProject.java
@@ -14,7 +14,13 @@ public class LoanProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+<<<<<<< .mine
+        // TODO code application logic here
+        // edit test
+    }
+=======
            // TODO code application logic here
-       // comment
+       // recomment 
  }
+>>>>>>> .r3
 }
