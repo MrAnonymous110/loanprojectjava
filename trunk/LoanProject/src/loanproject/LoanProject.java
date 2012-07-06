f66d777764a8a12d91892dfee26c973d0214e12a
ngthu92@gmail.com <ngthu92@gmail.com@f2ded979-f436-fa1c-38d0-fffca466db5f>
2012-07-06 13:35:28 +0000

    Test

--- trunk/LoanProject/src/loanproject/LoanProject.java
+++ trunk/LoanProject/src/loanproject/LoanProject.java
@@ -14,6 +14,7 @@ public class LoanProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+           // TODO code application logic here
+       // comment
+ }
 }
